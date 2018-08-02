99866ead9bb42b7b577a58e1101676349e2edc82
Felipe Souza <felipe.fabricio.m3@hotmail.com>
2018-08-02 11:25:24 -0300

    FORMATACAO DOS CAMPOS DE PRODUTOS E FORNECEDORES

--- SistemaMVC1/SistemaLojaMvcApi1/Models/Fornecedor.cs
+++ SistemaMVC1/SistemaLojaMvcApi1/Models/Fornecedor.cs
@@ -11,14 +11,23 @@ namespace SistemaLojaMvcApi1.Models
         [Key]
         public int FornecedorId { get; set; }
 
+        [Display(Name = "Nome")]
+        [Required(ErrorMessage = "Você precisa inserir o {0}")]
+        [StringLength(30, ErrorMessage = "Insira um nome de 1 a 50 caracteres.")]
         public string Nome { get; set; }
 
+        [Display(Name = "Sobrenome")]
         public string Sobrenome { get; set; }
 
+        [Display(Name = "Telefone")]
+        [Required(ErrorMessage = "Você precisa inserir o {0}")]
         public string Telefone { get; set; }
 
+        [Display(Name = "Endereço")]
         public string Endereco { get; set; }
 
+        [Display(Name = "Email")]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
         public virtual ICollection<FornecedorProduto> FornecedorProduto { get; set; }
--- SistemaMVC1/SistemaLojaMvcApi1/Models/Produto.cs
+++ SistemaMVC1/SistemaLojaMvcApi1/Models/Produto.cs
@@ -9,14 +9,28 @@ namespace SistemaLojaMvcApi1.Models
         [Key]
         public int ProdutoId { get; set; }
 
+        [Display(Name = "Nome")]
+        [Required(ErrorMessage = "Você precisa inserir o {0}")]
+        [StringLength(30, ErrorMessage = "Insira um nome de 1 a 50 caracteres.")]
         public string Descricao { get; set; }
 
+        [Display(Name = "Preço")]
+        [Required(ErrorMessage = "Você precisa inserir o {0}")]
+        [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
         public decimal Preco { get; set; }
 
+        [Display(Name = "Data da Compra")]
+        [Required(ErrorMessage = "Você precisa inserir a {0}")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime UltimaCompra { get; set; }
 
+        [Display(Name = "Estoque")]
+        [Required(ErrorMessage = "Você precisa inserir o {0}")]
+        [DisplayFormat(DataFormatString = "{0:N2}", ApplyFormatInEditMode = false)]
         public float Estoque { get; set; }
 
+        [Display(Name = "Comentário")]
+        [DataType(DataType.MultilineText)]
         public string Comentario { get; set; }
 
         //Migration Automatic - Aula 25
