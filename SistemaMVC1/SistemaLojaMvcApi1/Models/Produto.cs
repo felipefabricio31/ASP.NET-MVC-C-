89b592dd316e6f48b44c8d278bfc29f4f3122fd8
Felipe Souza <felipe.fabricio.m3@hotmail.com>
2018-08-02 10:15:57 -0300

    Primeiro Commit Entity Framework

---
+++ SistemaMVC1/SistemaLojaMvcApi1/Models/Produto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SistemaLojaMvcApi1.Models
+{
+    public class Produto
+    {
+        [Key]
+        public int ProdutoId { get; set; }
+
+        public string Descricao { get; set; }
+
+        public decimal Preco { get; set; }
+
+        public DateTime UltimaCompra { get; set; }
+
+        public float Estoque { get; set; }
+
+        public string Comentario { get; set; }
+
+        //Migration Automatic - Aula 25
+        //public string teste { get; set; }
+    }
+}
